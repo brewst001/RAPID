f9f7493d89aa842fffc085758a1fe5791cd65704
LindaTNguyen <nguylt1222@gmail.com>
2017-02-08 10:16:29 -0500

    Add script to migrate data to certificatesubscription table

---
+++ monitors/migrations/post/0004_migrate_subscriptions.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.apps import apps
+
+def populate_subscriptions(apps):
+    print("entering populate_subscriptions")
+    Certificates = apps.get_model("monitors","CertificateMonitor")
+    Subscription = apps.get_model("monitors","CertificateSubscription")
+    for certificate in Certificates.objects.all():
+       NewSubscription = Subscription(certificate=certificate,owner= certificate.owner)
+       NewSubscription.save()
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('monitors', '0003_certificatesubscription'),
+    ]
+
+    operations = [
+        migrations.RunPython(populate_subscriptions(apps)),
+    ]
