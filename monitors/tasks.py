c0c09a84d9dd6bbe448edf6ef9607821757becc9
LindaTNguyen <nguylt1222@gmail.com>
2017-03-22 11:23:35 -0400

    Create a new tasks.py for profiles to maintain any celery tasks

--- monitors/tasks.py
+++ monitors/tasks.py
@@ -486,24 +486,6 @@ class CertificateLookupSubTask(IndicatorLookupSubTask):
         return lookup
 
 
-
-class Update_Users(PeriodicTask):
-    """
-    Periodic task to update users as inactive who have not logged in for more than 90 days
-
-    This class runs every day at midnight
-    """
-    run_every = crontab(minute=0, hour=0)
-
-    def run(self, **kwargs):
-       print("Updating users alert flag...")
-       current_time = datetime.datetime.utcnow()
-       #print("current_time",current_time)
-       expired_date = current_time - datetime.timedelta(days=90)
-       print("expired_date:",expired_date)
-       Profile.objects.filter(last_login__lte = expired_date).update(alerts=False,is_active=False)
-
-
 class IndicatorMonitoring(PeriodicTask):
     """
     Monitor indicators as a periodic task.
---
+++ profiles/tasks.py
@@ -0,0 +1,33 @@
+"""
+Celery tasks for the User Profiles portion of RAPID.
+"""
+
+import datetime
+
+import logging
+
+from celery.schedules import crontab
+from celery.task import PeriodicTask
+from profiles.models import Profile
+
+LOGGER = logging.getLogger(None)
+"""The logger for this module"""
+# Note: Logging doesn't appear to work properly unless we get the root logger
+
+class Update_Users(PeriodicTask):
+    """
+    Periodic task to update users as inactive who have not logged in for more than 90 days
+
+    This class runs every day at midnight
+    """
+    #run_every = crontab(minute=0, hour=0)
+    run_every = crontab()
+
+    def run(self, **kwargs):
+       LOGGER.debug("Running Update_Users task...")
+       print("Updating users alert flag...")
+       current_time = datetime.datetime.utcnow()
+       #print("current_time",current_time)
+       expired_date = current_time - datetime.timedelta(days=90)
+       print("expired_date:",expired_date)
+       Profile.objects.filter(last_login__lte = expired_date).update(alerts=False,is_active=False)
