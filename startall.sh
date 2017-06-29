956ce24fde89922e07f526e70d80b647e5bfbdfc
LindaTNguyen <nguylt1222@gmail.com>
2017-06-29 14:57:00 -0400

    Update sudo service commands for celery tasks

--- startall.sh
+++ startall.sh
@@ -5,3 +5,9 @@
 /etc/init.d/celery_pivoteer start
 /etc/init.d/celery_daemon start
 /etc/init.d/celery_beat start
+
+#sudo service apache2 start
+
+#sudo service celery_pivoteer start
+#sudo service celery_daemon start
+#sudo service celery_beat start
--- stopall.sh
+++ stopall.sh
@@ -5,3 +5,9 @@
 /etc/init.d/celery_pivoteer stop
 
 /etc/init.d/apache2 stop
+
+#sudo service celery_beat stop
+#sudo service celery_daemon stop
+#sudo service celery_pivoteer stop
+
+#sudo service apache2 stop
