d8f289d35ec518fb3bd474f0aed67fbe63bd41d2
LindaTNguyen <nguylt1222@gmail.com>
2017-07-12 10:05:40 -0400

    Add monitor tag filters by user

--- core/templatetags/monitor_extras.py
+++ core/templatetags/monitor_extras.py
@@ -8,3 +8,10 @@ def digest(value):
 
     unique = hashlib.md5(value.encode('utf-8')).hexdigest()
     return unique
+
+
+@register.filter
+def filtertags(monitor,user):
+
+    result = monitor.filter(owner=user)
+    return result
