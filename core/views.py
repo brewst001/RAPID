080f8c1bbd552ad090126a3312934c9000ac5a67
LindaTNguyen <nguylt1222@gmail.com>
2017-08-01 13:10:10 -0400

    Default RAPID navigation path to Pivoting Tool form

--- core/views.py
+++ core/views.py
@@ -5,6 +5,7 @@ from django.core.urlresolvers import reverse
 from django.shortcuts import render, redirect
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
+from pivoteer.forms import SubmissionForm
 
 
 class HomePage(View):  # RedirectView
@@ -14,12 +15,17 @@ class HomePage(View):  # RedirectView
 
 
 class PrimaryNavigation(View):  # TemplateView
+  # Updated by: LNguyen
+  # Date: 1Aug2017
+  # Description: Update to default RAPID navigation path to the Pivoting Tool form
+  #   template_name = 'monitors/dashboard.html'
 
-    template_name = 'monitors/dashboard.html'
+    template_name = 'pivoteer/pivoteer.html'
+    template_vars = {'SubmissionForm': SubmissionForm}
 
     @method_decorator(login_required(login_url='login'))
     def get(self, request):
 
         request.dateval = datetime.datetime.utcnow()
 
-        return render(request, self.template_name)
+        return render(request, self.template_name, self.template_vars)
