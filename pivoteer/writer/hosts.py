0967bdb2d5ed35196ec3347d5cc025b445a8088f
LindaTNguyen <nguylt1222@gmail.com>
2017-08-08 12:38:18 -0400

    Update Historical export functionality to include PDNS data

--- pivoteer/writer/hosts.py
+++ pivoteer/writer/hosts.py
@@ -3,9 +3,10 @@ Classes and functions for writing Host Records.
 
 Host Records are IndicatorRecords with a record type of "HR."
 """
+import dateutil.parser
 
 from pivoteer.writer.core import CsvWriter
-
+from core.lookups import geolocate_ip
 
 class HostCsvWriter(CsvWriter):
     """
@@ -21,13 +22,57 @@ class HostCsvWriter(CsvWriter):
         super(HostCsvWriter, self).__init__(writer)
 
     def create_header(self):
-        return ["Date", "Source", "IP", "Domain", "IP Location"]
+        return ["Date", "Source", "Domain", "IP", "IP Location", "First Seen", "Last Seen"]
 
     def create_rows(self, record):
+
         if record is not None:
-            yield [record.info_date,
-                   record.info_source,
-                   record.info["ip"],
-                   record.info["domain"],
-                   record.info["geo_location"]]
+
+          if (record.info_source == 'PDS'):
+            for result in record.info['results']:
+
+                new_record = {
+                    'domain': result['domain'],
+                    'ip': result['ip'],
+                    'geo_location': geolocate_ip(result['ip']),
+                    'firstseen': dateutil.parser.parse(result['firstseen']),
+                    'lastseen': dateutil.parser.parse(result['lastseen'])
+                }
+
+                row = [
+                    record.info_date,
+                    record.info_source,
+                    new_record['domain'],
+                    new_record['ip'],
+                    new_record['geo_location'],
+                    new_record['firstseen'],
+                    new_record['lastseen']]
+
+                yield row
+
+          else:
+
+                new_record = {
+                    'domain': record.info["domain"],
+                    'ip': record.info["ip"],
+                    'geo_location': geolocate_ip(record.info["ip"]),
+                    'firstseen': record.created,
+                    'lastseen': ''
+                }
+
+                if ('firstseen' in record.info) and (record.info['firstseen'] != ''):
+                    new_record['firstseen'] = dateutil.parser.parse(record.info['firstseen'])
+                if ('lastseen' in record.info) and (record.info['lastseen'] != '') and (record.info['lastseen'] != {}):
+                    new_record['lastseen'] = dateutil.parser.parse(record.info['lastseen'])
+
+                row = [
+                    record.created,
+                    record.info_source,
+                    new_record['domain'],
+                    new_record['ip'],
+                    new_record['geo_location'],
+                    new_record['firstseen'],
+                    new_record['lastseen']]
+
+                yield row
 
