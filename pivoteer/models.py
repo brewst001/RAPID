4a6ab5974ffdbc12d03915469e75e34aa72649c1
LindaTNguyen <nguylt1222@gmail.com>
2017-08-15 13:09:50 -0400

    Limit Historical data by 100

--- pivoteer/models.py
+++ pivoteer/models.py
@@ -110,22 +110,22 @@ class IndicatorManager(models.Manager):
         time_frame = datetime.datetime.utcnow() + datetime.timedelta(hours=-24)
 
         if request.user.is_staff:
-            records = self.get_queryset().filter(~Q(info_source=RecordSource.PDS.name),
+            records = ( self.get_queryset().filter(~Q(info_source=RecordSource.PDS.name),
                                                  Q(record_type=record_type.name),
                                                  Q(info_date__lt=time_frame),
                                                  Q(info__icontains=indicator)) | \
                       self.get_queryset().filter(Q(info_source=RecordSource.PDS.name),
-                                                 Q(info__icontains=indicator))
+                                                 Q(info__icontains=indicator))).order_by('created')[:100]
 
         else:
-            records = self.get_queryset().filter(~Q(info_source=RecordSource.PTO.name),
+            records = ( self.get_queryset().filter(~Q(info_source=RecordSource.PTO.name),
                                                  ~Q(info_source=RecordSource.IID.name),
                                                  ~Q(info_source=RecordSource.PDS.name),
                                                  Q(record_type=record_type.name),
                                                  Q(info_date__lt=time_frame),
                                                  Q(info__icontains=indicator)) | \
                       self.get_queryset().filter(Q(info_source=record_source.PDS.name),
-                                                 Q(info__icontains=indicator))
+                                                 Q(info__icontains=indicator))).order_by('created')[:100]
 
         return records
 
