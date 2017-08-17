e43f4935fab2667f7799dd11febb14628e71d00c
LindaTNguyen <nguylt1222@gmail.com>
2017-08-17 15:21:49 -0400

    Remove conditions for staff users

--- pivoteer/models.py
+++ pivoteer/models.py
@@ -105,11 +105,10 @@ class IndicatorManager(models.Manager):
         record_type = RecordType.HR
         time_frame = datetime.datetime.utcnow() + datetime.timedelta(hours=-24)
 
-        if request.user.is_staff:
-            records = self.get_queryset().filter(Q(info_source=RecordSource.PDS.name),
-                                                 Q(record_type=record_type.name),
-                                                 Q(info_date__lt=time_frame),
-                                                 Q(info__icontains=indicator))
+        records = self.get_queryset().filter(Q(info_source=RecordSource.PDS.name),
+                                             Q(record_type=record_type.name),
+                                             Q(info_date__lt=time_frame),
+                                             Q(info__icontains=indicator))
 
         host_records_complete = []
 
@@ -148,11 +147,11 @@ class IndicatorManager(models.Manager):
         record_type = RecordType.HR
         time_frame = datetime.datetime.utcnow() + datetime.timedelta(hours=-24)
 
-        if request.user.is_staff:
-            historicalrecords = self.get_queryset().filter(~Q(info_source=RecordSource.PDS.name),
-                                                           Q(record_type=record_type.name),
-                                                           Q(info_date__lt=time_frame),
-                                                           Q(info__icontains=indicator))
+      #  if request.user.is_staff:
+        historicalrecords = self.get_queryset().filter(~Q(info_source=RecordSource.PDS.name),
+                                                        Q(record_type=record_type.name),
+                                                        Q(info_date__lt=time_frame),
+                                                        Q(info__icontains=indicator))
 
         host_records_complete = []
 
