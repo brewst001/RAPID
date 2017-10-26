4ae921dc13e98037d279d69c265eaa4f6978e394
root <root@nerestcnd0205c.cnd.local>
2017-10-26 09:01:22 -0400

    Default sorting to First Seen descending for Recent tab

--- static/pivoteer.js
+++ static/pivoteer.js
@@ -105,7 +105,7 @@ function initialize_table(record_element) {
             "bInfo": false,
             "bLengthChange": false,
             "bFilter": true,
-            "aaSorting": [[0, "desc"]]
+            "aaSorting": [[2, "desc"]]
         });
 
     }  else if (record_element == '#WhoIsPanel') {
