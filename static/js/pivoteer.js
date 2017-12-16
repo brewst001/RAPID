755d595ddd8b17899316f22770cf6dc0ea0f75f4
root <nguylt1222@gmail.com>
2017-12-16 09:44:34 -0500

    Hide Recent Cert tab

--- static/js/pivoteer.js
+++ static/js/pivoteer.js
@@ -11,7 +11,8 @@ function kill_ajax() {
 // Submit for all records
 function run_records(form_data) {
 
-    var records = [ "Recent", "RecentThreat",  "RecentCert", "WhoIs", "HistoricalDNS", "Historical", "Malware", "SafeBrowsing", "Search", "External", "DNSTwist" ];
+   // var records = [ "Recent", "RecentThreat",  "RecentCert", "WhoIs", "HistoricalDNS", "Historical", "Malware", "SafeBrowsing", "Search", "External", "DNSTwist" ];
+    var records = [ "Recent", "RecentThreat", "WhoIs", "HistoricalDNS", "Historical", "Malware", "SafeBrowsing", "Search", "External", "DNSTwist" ];
 
     $.each(records, function( index, record ) {
         // Clear out old information from DOM elements and submit new ajax requests
